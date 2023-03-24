d5ed234cc6f89e0b66ddbde351cc8fbae7258092
SotoTheMayor <quickfire25@yahoo.com>
2023-03-23 19:49:26 -0500

    passing all data needed from models in homepage

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,10 +1,68 @@
 const router = require('express').Router();
 const User = require('../models/user');
-const checkLogin = require('../utils/auth')
+const Post = require('../models/post');
+const Thread = require('../models/thread');
+const checkLogin = require('../utils/auth');
 
 // router.get('/', checkLogin, async (req, res) => {
 router.get('/', async (req, res) => {
-    res.render('homepage')
+    // if (req.session.loggedIn) {
+        const loggedUser = await User.findOne({
+            where: {id: req.session.user_id},
+            attributes: {exclude: ['password']}
+        })
+        try {
+            const communityData = await Post.findAll({
+                include: [
+                    {
+                        model: User,
+                        attributes: ['aliasName']
+                    },
+                    {
+                        model: Thread,
+                        attributes: ['thread', 'thread_offer', 'user_id']
+                    }
+                ]
+            });
+            const posts = communityData.map((post) =>
+                post.get({ plain:true })
+            );
+            res.render('homepage', {
+                posts,
+                aliasName: loggedUser.aliasName,
+                loggedIn: req.session.loggedIn,
+            })
+        } catch (err) {
+            console.log(err);
+            res.status(500).json(err);
+        }
+    // } else {
+    //     try {
+    //         const communityData = await Post.findAll({
+    //             include: [
+    //                 {
+    //                     model: User,
+    //                     attributes: ['aliasName']
+    //                 },
+    //                 {
+    //                     model: Thread,
+    //                     attributes: ['thread', 'thread_offer', 'user_id']
+    //                 }
+    //             ]
+    //         });
+    //         const posts = communityData.map((post) =>
+    //             post.get({ plain:true })
+    //         );
+    //         res.render('homepage', {
+    //             posts,
+    //             loggedIn: req.session.loggedIn,
+    //         })
+    //     } catch (err) {
+    //         console.log(err);
+    //         res.status(500).json(err);
+    //     }
+    // }
+    
 })
 
 
