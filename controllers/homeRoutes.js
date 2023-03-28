f1bd03c272afb6c3aefbfc041864995e3e166223
Mary <itsmaryyang@gmail.com>
2023-03-27 20:33:15 -0500

    Update userRoutes and profileRoutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,75 +1,66 @@
-const router = require('express').Router();
-// const User = require('../models/user');
-// const Post = require('../models/post');
-// const Thread = require('../models/thread');
-// const Category = require('../models/category');
-const { User, Post, Thread, Category } = require('../models')
-const checkLogin = require('../utils/auth');
+const router = require("express").Router();
+const { User, Post, Category, Thread } = require("../models");
+
+const checkLogin = require("../utils/auth");
 
 // router.get('/', checkLogin, async (req, res) => {
-router.get('/', async (req, res) => {
-    if (req.session.loggedIn) {
-        const loggedUser = await User.findOne({
-            where: {id: req.session.user_id},
-            attributes: {exclude: ['password']}
-        })
-        try {
-            const communityData = await Post.findAll({
-                include: [
-                    {
-                        model: User,
-                        attributes: ['aliasName']
-                    },
-                    {
-                        model: Thread,
-                        attributes: ['thread', 'counter_offer', 'user_id']
-                    }
-                ]
-            });
-            const posts = communityData.map((post) =>
-                post.get({ plain:true })
-            );
-            res.render('homepage', {
-                posts,
-                aliasName: loggedUser.aliasName,
-                loggedIn: req.session.loggedIn,
-            })
-        } catch (err) {
-            console.log(err);
-            res.status(500).json(err);
-        }
-    } else {
-        try {
-            const communityData = await Post.findAll({
-                include: [
-                    {
-                        model: User,
-                        attributes: ['aliasName']
-                    },
-                    {
-                        model: Thread,
-                        attributes: ['thread', 'counter_offer', 'user_id']
-                    },
-                    {
-                        model: Category,
-                        attributes: ['category']
-                    },
-                ]
-            });
-            const posts = communityData.map((post) =>
-                post.get({ plain:true })
-            );
-            res.render('homepage', {
-                posts,
-                // loggedIn: req.session.loggedIn,
-            })
-        } catch (err) {
-            console.log(err);
-            res.status(500).json(err);
-        }
+router.get("/", async (req, res) => {
+  if (req.session.loggedIn) {
+    const loggedUser = await User.findOne({
+      where: { id: req.session.user_id },
+      attributes: { exclude: ["password"] },
+    });
+    try {
+      const communityData = await Post.findAll({
+        include: [
+          {
+            model: User,
+            attributes: ["aliasName"],
+          },
+          {
+            model: Thread,
+            attributes: ["thread", "counter_offer", "user_id"],
+          },
+        ],
+      });
+      const posts = communityData.map((post) => post.get({ plain: true }));
+      res.render("homepage", {
+        posts,
+        aliasName: loggedUser.aliasName,
+        loggedIn: req.session.loggedIn,
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
     }
-    
-})
-
+  } else {
+    try {
+      const communityData = await Post.findAll({
+        include: [
+          {
+            model: User,
+            attributes: ["aliasName"],
+          },
+          {
+            model: Thread,
+            attributes: ["thread", "counter_offer", "user_id"],
+          },
+          {
+            model: Category,
+            attributes: ["category"],
+          },
+        ],
+      });
+      const posts = communityData.map((post) => post.get({ plain: true }));
+      res.render("homepage", {
+        posts,
+        // loggedIn: req.session.loggedIn,
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  }
+});
 
 module.exports = router;
--- controllers/profileRoutes.js
+++ controllers/profileRoutes.js
@@ -2,9 +2,29 @@ const router = require('express').Router();
 const User = require('../models/user');
 const checkLogin = require('../utils/auth')
 
-router.get('/', checkLogin, async (req, res) => {
-    res.render('profile')
-})
+
+router.get("/profile", checkLogin, async (req, res) => {
+  try {
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ["password"] },
+    });
+
+    const user = userData.get({ plain: true });
+
+    const postData = await Post.findAll({ where: { user_id: user_id } });
+
+    const posts = postData.map((post) => post.get({ plain: true }));
+
+    res.render("profile", {
+      ...user,
+      posts,
+      logged_in: true,
+    });
+  } catch (err) {
+    res.status(404).json(err);
+  }
+});
+
 
 
 module.exports = router;
