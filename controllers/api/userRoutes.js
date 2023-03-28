4fcb2d454ec12d9cc45fe06a70c98a990d62565e
SotoTheMayor <quickfire25@yahoo.com>
2023-03-27 19:55:03 -0500

    changes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const User  = require('../../models/user');
+const { User }  = require('../../models');
 
 // route for creating a new user login
 router.post('/', async (req, res) => {
